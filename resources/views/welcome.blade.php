299ad3047aff8b0f2525f8894fde55c18932d2ed
Alejandra <ale_arvizu93@hotmail.com>
2017-02-28 21:59:52 -0600

    tabla abogados

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Cotejos
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return 'welcome';
+    return 'cotejos';
 });
 
 Route::get('abogados', function () {
